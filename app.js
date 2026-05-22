500458e636f0d54427c6e1234bf15acda4a3924a
Rajeev Sharma <rajeevsharma@Rajeevs-MacBook-Air.local>
2026-05-22 13:43:20 +0530

    login webview

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const start = async () => {
       origin: true,
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+      allowedHeaders: ["Content-Type", "Authorization", "Accept", "Cache-Control", "Pragma", "Expires", "X-Requested-With"],
       preflightContinue: false,
       optionsSuccessStatus: 204
     });
